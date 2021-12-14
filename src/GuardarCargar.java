4ff96019e99d2164365cdc4d7a8f2b1d4cadb1e3
CinSab <73029945+CinSab@users.noreply.github.com>
2021-12-14 00:26:11 -0300

    Metodo Guardar

---
+++ src/GuardarCargar.java
@@ -0,0 +1,76 @@
+//import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.ParseException;
+import java.util.Iterator;
+
+public class GuardarCargar{
+   static void save(int[][] tableroFichas, boolean siguesuturno){
+        JSONObject estadoPartida = new JSONObject();
+        estadoPartida.put("Turno",siguesuturno);
+
+       JSONArray tabler = new JSONArray();
+       for(int i = 0; i < tableroFichas.length; i++){
+           for(int j = 0; j < tableroFichas.length; j++){
+               tabler.add(tableroFichas[i][j]);
+           }
+       }
+
+       /* JSONArray tabler = new JSONArray();
+        for(int i = 0; i < tablero.length; i++){
+            for(int j = 0; j < tablero.length; j++){
+                tabler.put(tablero[i][j]);
+            }
+        }
+        JSONArray pieza = new JSONArray();
+        for(int i = 0; i < piezas.length; i++){
+            for(int j = 0; j < piezas.length; j++){
+                tabler.put(piezas[i][j]);
+            }
+        }*/
+
+       //  tody.put("Posiciones",pieza);
+        estadoPartida.put("Tablero",tabler);
+
+        try (FileWriter file = new FileWriter("test.json")) {
+            file.write(estadoPartida.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(estadoPartida);
+
+    }
+
+    static  load(){
+        JSONParser parser = new JSONParser();
+        try {
+            Reader reader = new FileReader("test.json");
+            JSONObject partida = (JSONObject) parser.parse(reader);
+            System.out.println(partida);
+
+            boolean siguesuturno = (boolean) partida.get("Turno");
+            System.out.println(siguesuturno);
+
+            JSONArray tablero = (JSONArray) partida.get("Tablero");
+            Iterator<Object> iterator = tablero.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
